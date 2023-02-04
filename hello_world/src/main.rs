2600ada4d7aa586ef24a01cde7feb05a117ef249
Yasshi-cookie <y.teruyacookie@gmail.com>
2023-02-04 22:38:24 +0900

    『Rust言語入門』2.1まで

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    // 変数の中身を変更することは基本的に不可
+    // しかし、mut をつけると変更可能になる
+    // let mut message = "hello, foo";
+    let message;
+    if false {
+        message = "hello, foo";
+    } else {
+        message = "hello, bar";
+    }
+
+    println!("{message}");
+
+    // 再宣言はできる
+    let url = "https://rustup.rs/";
+    let url = "https://www.yahoo.co.jp/";
+
+    // データ型
+    // 文字型（1文字）と文字列の型の違いに注意
+    // 'で指定すると文字型になる。
+    let moji = 'a';
+    // "で指定すると文字列になる。
+    let mojiretsu = "mojiretsu";
+
+    // タプル型
+    let touple = ("hoge", 123);
+    println!("{}", touple.0);
 }
